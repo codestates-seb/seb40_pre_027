40a65c7eb10cd43cbcb4b3e1b8067b1079308b44
Gojeonghun <apple@appleui-MacBookPro.local>
2022-10-25 17:58:01 +0900

    ekk

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,15 @@
-import './App.css';
-import BasicExample from "./conponents/Button"
+import "./App.css";
+import Nav from "./conponents/home/Nav";
+import TagsBox from "./conponents/home/TagsBox";
+import BasicExample from "./conponents/Button";
 
 function App() {
   return (
     <div className="App">
       <BasicExample></BasicExample>
+      <Nav></Nav>
+      <TagsBox></TagsBox>
     </div>
-
   );
 }
 
--- client/src/conponents/Button.jsx
+++ client/src/conponents/Button.jsx
@@ -1,5 +1,5 @@
-import Button from 'react-bootstrap/Button';
-import ButtonGroup from 'react-bootstrap/ButtonGroup';
+import Button from "react-bootstrap/Button";
+import ButtonGroup from "react-bootstrap/ButtonGroup";
 
 function BasicExample() {
   return (
