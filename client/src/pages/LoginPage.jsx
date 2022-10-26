8025c1b0c78e8dddb8aef2766d430d2072d7a321
Gojeonghun <apple@appleui-MacBookPro.local>
2022-10-26 16:40:25 +0900

    로그인페이지 깃허브 구글 로그인 수정

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,112 +1,145 @@
-import React from "react";
-import styled from "styled-components";
-import Header from "../components/Header"
-import smallLogo from "../img/smallLogo.png"
-
-const LoginPageComponent = styled.div`
-.smallLogo{
-  width: 50px;
-  height: 50px;
-  margin-top: 40px;
-  margin-bottom: 20px;
+import React from 'react';
+import styled from 'styled-components';
+import Header from '../components/Header';
+import smallLogo from '../img/smallLogo.png';
+import { FcGoogle } from 'react-icons/fc';
+import { FaGithub } from 'react-icons/fa';
+{
+  /* <SignGoogle>
+<FcGoogle size={30} />
+&nbsp;&nbsp;Sign up with Google
+</SignGoogle>
+<SignGithub>
+<FaGithub size={30} />
+&nbsp;&nbsp;Sign up with Github
+</SignGithub> */
 }
-
-.account{
-  margin-top: 20px;
-  margin-bottom: 50px;
-}
-.signUp{
-  color: #0074cc;
-}
-
-background-color: #f1f2f3;
-height: 100%;
-width: 100%;
-padding: 100px;
-display: flex;
-flex-direction: column;
-align-items: center;
-justify-content: center;
-
-`
-
-
-const LoginBox = styled.div`
-.email{
-  font-size: 19px;
-}
-.pw{ 
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-}
-.password{
-  font-size: 19px;
-}
-.forgotPassword{
-  font-size: 14px;
-  color: #0074cc;
-}
-.inputBox{
-  width: 100%;
-  margin-bottom: 20px;
-}
-button{
-  width: 100%;
-  height: 37.8px;
-  background-color: #0a95ff;
-  color: #ffffff;
-  margin: 2px 0px;
-  padding: 10.4px;
-  border-radius: 5px;
-  border: 0;
-  outline: 0;
-  cursor: pointer;
-  
-
-}
-
-background-color: #ffffff;
-width: 288px;
-margin: 24px;
-padding: 24px;
-border-radius: 5%;
-`
 
 function LoginPage() {
-return (
+  const LoginPageComponent = styled.div`
+    .smallLogo {
+      width: 50px;
+      height: 50px;
+      margin-top: 40px;
+      margin-bottom: 20px;
+    }
+
+    .account {
+      margin-top: 20px;
+      margin-bottom: 50px;
+    }
+    .signUp {
+      color: #0074cc;
+    }
+
+    background-color: #f1f2f3;
+    height: 100%;
+    width: 100%;
+    padding: 100px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+  `;
+
+  const LoginBox = styled.div`
+    .email {
+      font-size: 19px;
+    }
+    .pw {
+      display: flex;
+      flex-direction: row;
+      justify-content: space-between;
+    }
+    .password {
+      font-size: 19px;
+    }
+    .forgotPassword {
+      font-size: 14px;
+      color: #0074cc;
+    }
+    .inputBox {
+      width: 100%;
+      margin-bottom: 20px;
+    }
+    button {
+      width: 100%;
+      height: 37.8px;
+      background-color: #0a95ff;
+      color: #ffffff;
+      margin: 2px 0px;
+      padding: 10.4px;
+      border-radius: 5px;
+      border: 0;
+      outline: 0;
+      cursor: pointer;
+    }
+
+    background-color: #ffffff;
+    width: 288px;
+    margin: 24px;
+    padding: 24px;
+    border-radius: 5%;
+  `;
+  const SignGoogle = styled.div`
+    width: 310px;
+    height: 38px;
+    margin: 15px;
+    border-radius: 5px;
+    text-align: center;
+    line-height: 2.3rem;
+    border: 1px solid white;
+    background-color: white;
+    font-size: 0.9rem;
+    cursor: pointer;
+  `;
+  const SignGithub = styled.div`
+    width: 310px;
+    height: 38px;
+    margin-bottom: 25px;
+    border-radius: 5px;
+    text-align: center;
+    line-height: 2.3rem;
+    border: 1px solid #2f3337;
+    background-color: #2f3337;
+    color: white;
+    font-size: 0.9rem;
+    cursor: pointer;
+  `;
+
+  return (
     <>
       <Header />
       <LoginPageComponent>
-        <img className='smallLogo' alt='stack-small-logo' src={smallLogo}></img>
+        <img className="smallLogo" alt="stack-small-logo" src={smallLogo}></img>
+        <SignGoogle>
+          <FcGoogle size={30} />
+          &nbsp;&nbsp;Sign up with Google
+        </SignGoogle>
+        <SignGithub>
+          <FaGithub size={30} />
+          &nbsp;&nbsp;Sign up with Github
+        </SignGithub>
         <LoginBox>
-          <div className='email'>Email</div>
-          <input className='inputBox'></input>
-          <div className='pw'>
-          <div className='password'>Password</div>
-          <div className='forgotPassword'>Forgot Password?</div>
+          <div className="email">Email</div>
+          <input className="inputBox"></input>
+          <div className="pw">
+            <div className="password">Password</div>
+            <div className="forgotPassword">Forgot Password?</div>
           </div>
-          <input className='inputBox'></input>
+          <input className="inputBox"></input>
           <button>Log in</button>
         </LoginBox>
-        <div className='account'>
+        <div className="account">
           <span>Don't have an account?</span>
-          <span className='signUp'> Sign up</span>
+          <span className="signUp"> Sign up</span>
         </div>
-
       </LoginPageComponent>
-      
     </>
-  )
+  );
 }
 
 export default LoginPage;
-
-
-
-
-
-
 
 // import React from "react";
 // import styled from "styled-components";
@@ -148,7 +181,7 @@ export default LoginPage;
 //       background-color: #2f3337;
 
 //     }
-    
+
 //     padding: 40px;
 //     height: 100vh;
 //     display: flex;
@@ -167,10 +200,10 @@ export default LoginPage;
 //       <LoginPageComponent>
 //         <img className='smallLogo' alt='stack-small-logo' src={smallLogo}></img>
 //         <button className='googleButton'>Log in with Google</button>
-//         <button className='githubButton'>Log in with GitHub</button> 
-          
+//         <button className='githubButton'>Log in with GitHub</button>
+
 //       </LoginPageComponent>
-      
+
 //     </>
 //   )
 // }
