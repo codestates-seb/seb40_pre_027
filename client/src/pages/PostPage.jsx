011146b7492e9d445e627104541b8c7475898c91
Gojeonghun <apple@appleui-MacBookPro.local>
2022-10-27 17:38:50 +0900

    포스트 타이틀 수정

--- client/src/pages/PostPage.jsx
+++ client/src/pages/PostPage.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import Title from '../components/post/Title';
+
 function PostPage() {
-  return <Title></Title>;
+  return <div>PostPage</div>;
 }
 
 export default PostPage;
