fdfd7f507d2525023faae36d50723e0053b42d4d
starplanter93 <starplanter93@gmail.com>
2022-10-27 16:43:55 +0900

    PostPage 바디 컴포넌트 ui 구현

--- client/src/pages/PostPage.jsx
+++ client/src/pages/PostPage.jsx
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from 'react';
 
 function PostPage() {
-  return (
-    <div>PostPage</div>
-  )
+  return <div>PostPage</div>;
 }
 
-export default PostPage
+export default PostPage;
