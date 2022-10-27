c1e966f2dadee05cc1e95a489c04446c2e53f4b9
Aurora <eunbipower@gmail.com>
2022-10-27 16:42:59 +0900

    Recommend + RecommendPopup

--- client/src/pages/PostPage.jsx
+++ client/src/pages/PostPage.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import Recommend from '../components/post/Recommend'
+import RecommendPopup from '../components/post/RecommendPopup';
 
 function PostPage() {
-  return <div>PostPage</div>;
+  return <div>
+    <Recommend />
+    <RecommendPopup />
+  </div>;
 }
 
 export default PostPage;
