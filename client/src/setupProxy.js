4bb32fcdf56548762c2dd72577aa130a2f241fc9
starplanter93 <starplanter93@gmail.com>
2022-10-31 16:10:27 +0900

    프록시설정 endpoint 배열로 묶음

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,22 +1,8 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
-const url = 'https://5b03-175-207-149-117.jp.ngrok.io'
+const url = 'https://5b03-175-207-149-117.jp.ngrok.io';
 module.exports = (app) => {
   app.use(
-    '/user',
-    createProxyMiddleware({
-      target: url,
-      changeOrigin: true,
-    })
-  );
-  app.use(
-    '/question',
-    createProxyMiddleware({
-      target: url,
-      changeOrigin: true,
-    })
-  );
-  app.use(
-    '/answer',
+    ['/user', '/question', '/answer'],
     createProxyMiddleware({
       target: url,
       changeOrigin: true,
